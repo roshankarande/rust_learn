e633ec03137f87c26d973ec83463eb2b4545cc88
roshankarande <roshan.1.6180@gmail.com>
2024-04-22 08:54:35 -0700

    .

---
+++ embed/src/main.rs
@@ -0,0 +1,44 @@
+use axum::Router;
+use std::net::SocketAddr;
+use tower_http::{
+    services::ServeDir,
+    // trace::TraceLayer,
+};
+// use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+
+    tokio::join!(
+        serve(using_serve_dir(), 3001),
+    );
+}
+
+fn using_serve_dir() -> Router {
+    // serve the file in the "assets" directory under `/assets`
+    Router::new().nest_service("/assets", ServeDir::new("static"))
+}
+
+
+async fn serve(app: Router, port: u16) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app)
+    .await
+    .unwrap();
+
+
+    // tracing_subscriber::registry()
+    //     .with(
+    //         tracing_subscriber::EnvFilter::try_from_default_env()
+    //             .unwrap_or_else(|_| "example_static_file_server=debug,tower_http=debug".into()),
+    //     )
+    //     .with(tracing_subscriber::fmt::layer())
+    //     .init();
+
+    // axum::serve(listener, app.layer(TraceLayer::new_for_http()))
+    //     .await
+    //     .unwrap();
+}
